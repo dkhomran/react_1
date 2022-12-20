bb44871365ba47b95a3858a66dc05bc1de476f63
Dekhil Omran <dekhil22omran@gmail.com>
2022-12-20 14:50:23 +0100

    2 comm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form className="container" action="test.php">
+        <div>
+          <label htmlFor="user">
+            Username <sup>*</sup>
+          </label>
+          <input
+            name="User"
+            id="user"
+            type="text"
+            placeholder="Type Your Name In English Only"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="pass">
+            Password <sup>*</sup>
+          </label>
+          <input
+            name="Pass"
+            id="pass"
+            type="password"
+            placeholder="Type a Complex Password"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="mail">
+            Email <sup>*</sup>
+          </label>
+          <input
+            name="Email"
+            id="mail"
+            type="email"
+            placeholder="Type a Valid Email"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone</label>
+          <input
+            name="Phone"
+            id="phone"
+            type="tel"
+            pattern="[0-9]{2}-[0-9]{3}-[0-9]{3}"
+          />
+        </div>
+        <div>
+          <label htmlFor="brief">Brieft</label>
+          <textarea
+            name="brief"
+            id="brief"
+            cols={30}
+            rows={10}
+            defaultValue={""}
+          />
+        </div>
+        <div>
+          <input type="submit" defaultValue="Register" />
+        </div>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
